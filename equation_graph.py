8b91c08136370565e74eb905395606b9c06ae068
n-k-t <nkt8@case.edu>
2023-06-29 23:19:45 -0400

    Added in a basic equation tree to svg tool. It will intake a tensor and traverse its tree of operations and parent tensors, creating a directed graph along the way using NetworkX. There are simple labels and colors to identify where weights and values are instantiated from where operations take place.

---
+++ equation_graph.py
@@ -0,0 +1,31 @@
+from tensor import Tensor, Operator
+from operators import *
+
+import networkx as nx
+import os
+
+operator_labels = {
+    Addition: 'ADD', 
+    Subtraction: 'SUB', 
+    MatrixMultiplication: 'MATMUL'
+}
+
+def to_svg(root):
+    topological = root.topological_sort()
+    G = nx.DiGraph()
+    for tensor in topological:
+        parents = [*tensor.operator.parents]
+        G.add_edge(str(parents[0]), str(tensor), label = operator_labels[tensor.operator.__class__])
+        G.add_edge(str(parents[1]), str(tensor), label = operator_labels[tensor.operator.__class__])
+        G.nodes[str(tensor)]['fillcolor'] = '#FFCCCB'
+        G.nodes[str(tensor)]['color'] = '#000000'
+        G.nodes[str(tensor)]['style'] = 'filled'
+        G.nodes[str(tensor)]['label'] = 'Tensor'
+        for i in parents:
+            if i.operator == None:
+                G.nodes[str(i)]['fillcolor'] = '#32CD32'
+                G.nodes[str(i)]['color'] = '#000000'
+                G.nodes[str(i)]['style'] = 'filled'
+                G.nodes[str(i)]['label'] = 'Instantiate'
+    nx.drawing.nx_pydot.write_dot(G, f'equation_graph.dot')
+    os.system(f'dot -Tsvg equation_graph.dot -o equation_graph.svg')
