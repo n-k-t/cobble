18cc6f35c51dfd6166645386e92467857c9b906d
n-k-t <nkt8@case.edu>
2023-06-30 10:46:43 -0400

    Updated the graph tool to highlight the output node.

--- equation_graph.py
+++ equation_graph.py
@@ -12,15 +12,22 @@ operator_labels = {
 # Will break if there are less than two parents. Should fix for when activation functions are added in.
 def to_svg(root) -> 'None':
     topological = root.topological_sort()
+    topological.reverse()
     G = nx.DiGraph()
-    for tensor in topological:
+    for count, tensor in enumerate(topological):
         parents = [*tensor.operator.parents]
         G.add_edge(str(parents[0]), str(tensor), label = operator_labels[tensor.operator.__class__])
         G.add_edge(str(parents[1]), str(tensor), label = operator_labels[tensor.operator.__class__])
-        G.nodes[str(tensor)]['fillcolor'] = '#FFCCCB'
-        G.nodes[str(tensor)]['color'] = '#000000'
-        G.nodes[str(tensor)]['style'] = 'filled'
-        G.nodes[str(tensor)]['label'] = 'Tensor'
+        if count == 0:
+            G.nodes[str(tensor)]['fillcolor'] = '#FFF000'
+            G.nodes[str(tensor)]['color'] = '#000000'
+            G.nodes[str(tensor)]['style'] = 'filled, dashed'
+            G.nodes[str(tensor)]['label'] = 'Output'
+        else:
+            G.nodes[str(tensor)]['fillcolor'] = '#FFCCCB'
+            G.nodes[str(tensor)]['color'] = '#000000'
+            G.nodes[str(tensor)]['style'] = 'filled'
+            G.nodes[str(tensor)]['label'] = 'Tensor'
         for i in parents:
             if i.operator == None:
                 G.nodes[str(i)]['fillcolor'] = '#32CD32'
